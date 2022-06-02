a006df73a4de021b82027337529054a2d15bc351
missozoda <ozoshka1207@gmail.com>
2022-06-02 21:09:54 +0500

    add some changes

--- js/script.js
+++ js/script.js
@@ -15,11 +15,9 @@ let elModalTemplate = $("#modal-template").content;
 
 // Apini fetch qilish
 let callFetchApi = function(api){
-  fetch(api)
-  .then(function(response){
+  fetch(api).then(function(response){
     return response.json();
-  })
-  .then(function(data){
+  }).then(function(data){
     console.log(data.articles);
     renderNews(data.articles)
     return data.articles;
@@ -38,8 +36,7 @@ elForm.addEventListener("submit", function(e){
     if(!search == ""){
       let api = `https://newsapi.org/v2/everything?q=${search}&sortBy=${sortBy}&apiKey=23b7a8c33c5841c8acaaca94327ff30f`;
       return api;
-    }
-    else{
+    }else{
       api = FetchApi;
       return api;
     }
@@ -52,7 +49,9 @@ let renderNews = (news) => {
   let elResultFragment = document.createDocumentFragment();
   let createNewsItem = (onlyNew) => {
     elResultList.innerHTML = "";
+
     let elNewLi = elTemplate.cloneNode(true);
+    let elNewModal = elModalTemplate.cloneNode(true);
 
     $(".news-img", elNewLi).src = onlyNew.urlToImage;
     $(".news-img", elNewLi).alt = onlyNew.title;
@@ -61,14 +60,18 @@ let renderNews = (news) => {
     $(".date", elNewLi).textContent = onlyNew.publishedAt.split("T").splice(0, 1);
     $(".source",elNewLi).href = onlyNew.url;
     $(".content", elNewLi).textContent = onlyNew.content.split("[").shift();
+    $(".more-btn", elNewLi).setAttribute("data-bs-target", `#exampleModal${onlyNew.publishedAt.split(":").shift()}`);
+
+    $(".more-modal", elNewModal).id = `exampleModal${onlyNew.publishedAt.split(":").shift()}`
+    $(".title", elNewModal).textContent = onlyNew.title;
+    $(".description", elNewModal).textContent = onlyNew.description;
+    elNewLi.appendChild(elNewModal);
     return elNewLi;
   }
 
   news.forEach ((onlyNew) => {
     elResultFragment.appendChild(createNewsItem(onlyNew));
   })
-
+  
   elResultList.appendChild(elResultFragment);
 }
-
-// renderNews(callFetchApi(FetchApi));
